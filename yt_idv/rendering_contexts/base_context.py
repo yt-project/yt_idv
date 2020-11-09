d239970f66bf16fba787648ee35618bbd3f7c596
Matthew Turk <matthewturk@gmail.com>
2020-11-09 14:48:46 -0600

    Add an add_image method for a widget

--- yt_idv/rendering_contexts/base_context.py
+++ yt_idv/rendering_contexts/base_context.py
@@ -10,3 +10,9 @@ class BaseContext:
 
         self.scene = SceneGraph.from_ds(ds, field, no_ghost=no_ghost)
         return self.scene
+
+    def add_image(self):
+        import ipywidgets
+
+        self.image_widget = ipywidgets.Image(height=self.height, width=self.width)
+        return self.image_widget
