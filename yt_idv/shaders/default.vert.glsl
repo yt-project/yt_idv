4ca95f713d28ac1615235dcbac12145232bf5df9
Matthew Turk <matthewturk@gmail.com>
2020-12-02 14:53:00 -0600

    Fixing interpolation state of some variables

--- yt_idv/shaders/default.vert.glsl
+++ yt_idv/shaders/default.vert.glsl
@@ -3,8 +3,8 @@ in vec4 model_vertex; // The location of the vertex in model space
 in vec3 in_dx;
 in vec3 in_left_edge;
 in vec3 in_right_edge;
-out vec4 v_model;
-out vec3 v_camera_pos;
+flat out vec4 v_model;
+flat out vec3 v_camera_pos;
 flat out mat4 inverse_proj;
 flat out mat4 inverse_mvm;
 flat out mat4 inverse_pmvm;
