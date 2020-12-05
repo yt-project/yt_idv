8a6a9968e114dc41801c135e101b43851088a7f0
Matthew Turk <matthewturk@gmail.com>
2020-12-05 15:50:47 -0600

    Start adding tests and reduce deprecation warnings.

--- tests/test_yt_idv.py
+++ tests/test_yt_idv.py
@@ -2,35 +2,37 @@
 
 """Tests for `yt_idv` package."""
 
+import os
+
 import pytest
-from click.testing import CliRunner
+import yt
 
-# from yt_idv import yt_idv
-from yt_idv import cli
+import yt_idv
+
+os.environ["PYOPENGL_PLATFORM"] = "egl"
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+@pytest.fixture()
+def egl_fake_amr():
+    """Return an EGL context that has a "fake" AMR dataset added, with "radius"
+    as the field.
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    ds = yt.testing.fake_amr_ds()
+    dd = ds.all_data()
+    rc = yt_idv.render_context("egl", width=1024, height=1024)
+    rc.add_scene(dd, "radius", no_ghost=True)
+    return rc
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+@pytest.fixture()
+def egl_empty():
+    """Return an EGL context that has no dataset.
+    """
+    rc = yt_idv.render_context("egl", width=1024, height=1024)
+    return rc
 
 
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert "yt_idv.cli.main" in result.output
-    help_result = runner.invoke(cli.main, ["--help"])
-    assert help_result.exit_code == 0
-    assert "--help  Show this message and exit." in help_result.output
+def test_snapshots(egl_fake_amr):
+    """Check that we can make some snapshots."""
+    image = egl_fake_amr.run()
+    yt.write_bitmap(image, "test.png")
