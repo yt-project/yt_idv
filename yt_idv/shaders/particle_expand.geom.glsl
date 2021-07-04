4f82330ca3959756dc55d7cecbc15e0ca8189817
Matthew Turk <matthewturk@gmail.com>
2021-07-03 20:10:26 -0500

    Do the expansion via inverse matrices

--- yt_idv/shaders/particle_expand.geom.glsl
+++ yt_idv/shaders/particle_expand.geom.glsl
@@ -22,48 +22,28 @@ flat in vec3 vv_camera_pos[];
 // https://stackoverflow.com/questions/28375338/cube-using-single-gl-triangle-strip
 // suggests that the triangle strip we want for the cube is
 
-uniform vec3 arrangement[8] = vec3[](
-    vec3(-1, -1, -1),
-    vec3(1, -1, -1),
-    vec3(-1, 1, -1),
-    vec3(1, 1, -1),
-    vec3(-1, -1, 1),
-    vec3(1, -1, 1),
-    vec3(-1, 1, 1),
-    vec3(1, 1, 1)
-);
-
 void main() {
 
     vec4 cornerPos;
     vec2 cornerFrag;
     vec4 position = vv_model[0];
-
     mat4 pmvm = projection * modelview;
 
-    vec2 fragLeftCorner, fragRightCorner;
-    fragLeftCorner = fragRightCorner = gl_in[0].gl_Position.xy;
-
-    // This is definitely not the smartest way to do this!  We should be
-    // computing something in the clip/normal space.  We can take advantage of
-    // the fact that our sphere will always have at least one radial vector that
-    // is perpendicular to the eye vector.
-
-    for (int i = 0; i < 8; i++) { 
-        cornerPos = position + vec4(arrangement[i], 0.0) * vradius[0];
-        cornerFrag = (projection * modelview * cornerPos).xy;
-        fragLeftCorner.x = min(fragLeftCorner.x, cornerFrag.x);
-        fragLeftCorner.y = min(fragLeftCorner.y, cornerFrag.y);
-        fragRightCorner.x = max(fragRightCorner.x, cornerFrag.x);
-        fragRightCorner.y = max(fragRightCorner.y, cornerFrag.y);
-    }
+    // This is our inverse projection model view matrix.  That means that if we
+    // move something in clip space and multiply by this, it becomes the world
+    // coordinates of that new thing.
+    //
+    // We want to figure out what the world vectors corresponding to clip space
+    // motion (at the z and w of our current vertex) are.
+    vec4 fragLeftCorner = pmvm * (position + vradius[0] * normalize(vinverse_pmvm[0] * vec4(-1.0, -1.0, gl_in[0].gl_Position.zw)));
+    vec4 fragRightCorner = pmvm * (position + vradius[0] * normalize(vinverse_pmvm[0] * vec4(1.0, 1.0, gl_in[0].gl_Position.zw)));
 
     // We don't want to allow each particle to be bigger than some maximum size
     // in clip space coordinates.
+    
     fragLeftCorner.x = max(fragLeftCorner.x, gl_in[0].gl_Position.x - max_particle_size/2.0);
-    fragRightCorner.x = min(fragRightCorner.x, gl_in[0].gl_Position.x + max_particle_size/2.0);
-
     fragLeftCorner.y = max(fragLeftCorner.y, gl_in[0].gl_Position.y - max_particle_size/2.0);
+    fragRightCorner.x = min(fragRightCorner.x, gl_in[0].gl_Position.x + max_particle_size/2.0);
     fragRightCorner.y = min(fragRightCorner.y, gl_in[0].gl_Position.y + max_particle_size/2.0);
 
     // https://stackoverflow.com/questions/12239876/fastest-way-of-converting-a-quad-to-a-triangle-strip
