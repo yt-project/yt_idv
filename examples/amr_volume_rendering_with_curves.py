427695c385a3da943afbe2b0c4425ffe8fe83d6a
chrishavlin <chris.havlin@gmail.com>
2021-08-26 17:21:53 -0500

    curve example using streamlines now

--- examples/amr_volume_rendering_with_curves.py
+++ examples/amr_volume_rendering_with_curves.py
@@ -1,5 +1,7 @@
 import numpy as np
 import yt
+from yt.units import Mpc
+from yt.visualization.api import Streamlines
 
 import yt_idv
 from yt_idv.scene_components.curves import (  # NOQA
@@ -10,32 +12,46 @@ from yt_idv.scene_data.curve import CurveCollection, CurveData  # NOQA
 
 ds = yt.load_sample("IsolatedGalaxy")
 
+# generate some streamlines
+# (see https://yt-project.org/doc/visualizing/streamlines.html)
+c = ds.domain_center  # the center of the box
+N = 10  # number of streamlines
+scale = ds.domain_width[0]  # scale of streamlines relative to boxsize
+pos_dx = np.random.random((N, 3)) * scale - scale / 2.0
+pos = c + pos_dx  # starting positions
+
+streamlines = Streamlines(
+    ds,
+    pos,
+    ("gas", "velocity_x"),
+    ("gas", "velocity_y"),
+    ("gas", "velocity_z"),
+    length=1.0 * Mpc,
+    get_magnitude=True,
+)
+
+streamlines.integrate_through_volume()
+
+# initialize the rendering context and scene
 rc = yt_idv.render_context(height=800, width=800, gui=True)
 sg = rc.add_scene(ds, "density", no_ghost=True)
 
-# partial circle in the x-y plane in model space
-r = 0.25
-c = np.array([0.5, 0.5, 0.5])
-theta = np.linspace(0, 2 * np.pi * 0.75, 50)
-x = r * np.cos(theta) + c[0]
-y = r * np.sin(theta) + c[1]
-z = np.full(x.shape, c[2])
-curve = np.column_stack([x, y, z])
 
+# add a single streamline as a single curve
 curved = CurveData()
-curved.add_data(curve)
-curver = CurveRendering(data=curved, curve_rgba=(1.0, 0.0, 0.0, 1.0))
+curved.add_data(streamlines.streamlines[0])
+curve_render = CurveRendering(data=curved, curve_rgba=(1.0, 0.0, 0.0, 1.0))
 rc.scene.data_objects.append(curved)
-rc.scene.components.append(curver)
+rc.scene.components.append(curve_render)
 
-# a collection of curves
+# add the remaining streamlines as a collection of curves
 curve_collection = CurveCollection()
-curve_collection.add_curve(np.column_stack([x - 0.25, y - 0.25, z - 0.25]))
-curve_collection.add_curve(np.column_stack([x + 0.25, y + 0.25, z + 0.25]))
-curve_collection.add_data()
+for stream in streamlines.streamlines[1:]:
+    stream = stream[np.all(stream != 0.0, axis=1)]
+    curve_collection.add_curve(stream)
+curve_collection.add_data()  # call add_data() after done adding curves
 
 cc_render = CurveCollectionRendering(data=curve_collection)
-
 rc.scene.data_objects.append(curve_collection)
 rc.scene.components.append(cc_render)
 
