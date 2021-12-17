85ad0204c786b513038aec74986680e0b1334aef
Chris Havlin <chris.havlin@gmail.com>
2021-12-17 11:42:21 -0600

    fix comment

--- yt_idv/gui_support.py
+++ yt_idv/gui_support.py
@@ -1,6 +1,5 @@
 def add_popup_help(imgui, message: str):
     # adds a hover-triggered popup message to the last-defined imgui item
-    # seems to work without passing in the imgui context
     if imgui.is_item_hovered():
         imgui.begin_tooltip()
         imgui.text(message)
