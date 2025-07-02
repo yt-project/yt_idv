0ab53eed88a3cb6d9ebc7fce371cb721bd3fa8e7
chrishavlin <chris.havlin@gmail.com>
2025-07-02 14:28:21 -0500

    add example of rendering halo catalog as sph ds

---
+++ examples/halo_rendering_as_sph.py
@@ -0,0 +1,47 @@
+import os
+
+import yt
+from yt.extensions.astro_analysis.halo_analysis import HaloCatalog
+
+import yt_idv
+from yt_idv.scene_components.sph_particles import SPHRendering
+from yt_idv.scene_data.particle_positions import ParticlePositions
+
+# ds = yt.load_sample("Enzo_64")
+ds = yt.load("Enzo_64/DD0025/data0025")
+
+hc = HaloCatalog(data_ds=ds, finder_method="fof")
+hc.create()
+
+# set the halo file to load
+files = os.listdir(hc.output_dir)
+files.sort()
+for fi in files:
+    if hc.output_basename in fi:
+        halo_file = os.path.join(hc.output_dir, fi)
+
+# loaded as
+ds_halos = yt.load(halo_file)
+
+
+dd = ds_halos.all_data()
+
+print(dd.quantities.extrema(("halos", "virial_radius")))
+rc = yt_idv.render_context(height=800, width=800, gui=True)
+sg = rc.add_scene(ds, None, no_ghost=True)
+
+pos = ParticlePositions(
+    data_source=dd,
+    radius_field="virial_radius",
+    particle_type="halos",
+    color_field="virial_radius",
+    position_field="particle_position",
+)
+pren: SPHRendering = SPHRendering(data=pos, scale=1.0)
+pren.max_particle_size = 0.000000001
+
+sg.data_objects.append(pos)
+sg.components.append(pren)
+
+print(sg.camera.near_plane, sg.camera.far_plane)
+rc.run()
