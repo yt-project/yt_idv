f684cc3fbe73b21d5c52009962bf0cf3d3b58c10
chavlin <chris.havlin@gmail.com>
2026-08-28 10:11:36 -0500

    make test canvas size configurable, drop macos CI to 256x256

--- yt_idv/tests/conftest.py
+++ yt_idv/tests/conftest.py
@@ -26,6 +26,18 @@ def pytest_addoption(parser):
         help="default offscreen rendering backend, if --offscreen-backend is unset",
         default="osmesa",
     )
+    parser.addoption(
+        "--canvas-width",
+        type=int,
+        default=1024,
+        help="default width of offscreen rendering canvases",
+    )
+    parser.addoption(
+        "--canvas-height",
+        type=int,
+        default=1024,
+        help="default height of offscreen rendering canvases",
+    )
 
 
 def _resolve_backend(config):
@@ -48,11 +60,19 @@ def pytest_configure(config):
     # this will get run before all tests, before collection and
     # any opengl imports that happen within test files.
     config._offscreen_backend = _resolve_backend(config)
+    config._canvas_size = (
+        config.getoption("--canvas-width"),
+        config.getoption("--canvas-height"),
+    )
     prepare_platform(config._offscreen_backend)
 
 
 def pytest_report_header(config):
-    return f"offscreen backend: {config._offscreen_backend}"
+    width, height = config._canvas_size
+    return (
+        f"offscreen backend: {config._offscreen_backend}, "
+        f"default canvas size: {width}x{height}"
+    )
 
 
 @pytest.fixture(scope="session")
@@ -65,7 +85,10 @@ def render_backend(request):
 def make_rc(render_backend, request):
     """yield a factory for rendering contexts that get destroyed after the test"""
 
-    def _make_rc(width=1024, height=1024, **kwargs):
+    def _make_rc(width=None, height=None, **kwargs):
+        default_width, default_height = request.config._canvas_size
+        width = default_width if width is None else width
+        height = default_height if height is None else height
         kwargs = {**_BACKEND_KWARGS.get(render_backend, {}), **kwargs}
         rc = yt_idv.render_context(render_backend, width=width, height=height, **kwargs)
         request.addfinalizer(rc.close)
